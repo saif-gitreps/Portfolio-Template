e89dd6a97ad56bd5cee0e1cf152026549964869f
Saif <droconos99@gmail.com>
2024-03-11 19:46:35 +0600

    finished home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Home() {
    return (
       <div className="mx-auto w-full max-w-7xl">
-         <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
-            <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-               <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+         <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-1">
+            <div className="relative z-10 max-w-screen-xl py-20 my-20 mx-auto sm:px-6 lg:px-8">
+               <div className="inset-0 w-full sm:my-1 sm:pt-1 pt-12 ">
                   <div className="wrapper">
                      <div className="typing-demo">Hello Saif here,</div>
                   </div>
@@ -28,18 +28,34 @@ export default function Home() {
                </div>
             </div>
          </aside>
-
-         <div className="grid  place-items-center sm:mt-20">
-            <img
-               className="sm:w-96 w-48"
-               src="https://i.ibb.co/2M7rtLk/Remote1.png"
-               alt="image2"
-            />
+         <div className="relative overflow text-black rounded-lg sm:mx-16 mx-2 sm:py-10">
+            <h2 className="text-3xl font-bold sm:text-4xl">Technologies I work with..</h2>
+            <ul className="text-2xl ">
+               <li>
+                  {" "}
+                  <span className="text-blue-900 font-extrabold ml-6">
+                     Languages:
+                  </span>{" "}
+                  Python, C++, JavaScript, ASM86, PHP, Java
+               </li>
+               <li>
+                  {" "}
+                  <span className="text-blue-900 font-extrabold ml-8">Frameworks: </span>
+                  Express.js, React.js, Flask.py, Mongoose.js , Prisma.js , EJS, FastAPI
+               </li>
+               <li>
+                  {" "}
+                  <span className="text-blue-900 font-extrabold ml-10">Databases: </span>
+                  MongoDB, MySQL, PostreSQL
+               </li>
+               <li>
+                  <span className="text-blue-900 font-extrabold ml-12">
+                     Cloud and Tools:{" "}
+                  </span>{" "}
+                  Render, AWS, Git/Github, Postman, Linux
+               </li>
+            </ul>
          </div>
-
-         <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
-            Lorem Ipsum Yojo
-         </h1>
       </div>
    );
 }
