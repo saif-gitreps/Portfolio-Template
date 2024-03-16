4f75339efc21cafcb9f5bd0602ad1cb80c5c913c
Saif <droconos99@gmail.com>
2024-03-17 02:37:56 +0600

    Card design finalised

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -4,9 +4,9 @@ export default function Github() {
    const data = useLoaderData();
    return (
       <div className="flex justify-center flex-col md:flex-row items-center py-10">
-         <div className="card m-10">
-            <div className="card__img"></div>
-            <p className="card__title text-2xl font-semibold">Project</p>
+         <div className="card m-10 ">
+            <div className="card__img bg-blue-800"></div>
+            <p className="card__title text-3xl font-semibold">Project</p>
             <div className="card__descr-wrapper flex-col justify-between h-full">
                <p className="card__descr text-xl">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
@@ -17,37 +17,18 @@ export default function Github() {
                   Facilis!
                </p>
                <div className="card__links text-xl">
-                  <div>
-                     <a className="link text-blue-800 font-semibold" href="#">
-                        <img src="src/assets/icons8-github-96.png" />
-                        Code
+                  <div className="flex flex-row">
+                     <img src="src/assets/icons8-github-96.png" className="w-7 h-7" />
+                     <a
+                        className="link text-blue-800 font-semibold flex items-center"
+                        href="#"
+                     >
+                        Source Code
                      </a>
                   </div>
                   <div>
                      <a className="link text-blue-800 font-semibold" href="#">
                         Preview
-                     </a>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div className="card m-10">
-            <div className="card__img"></div>
-            <p className="card__title">Project</p>
-            <div className="card__descr-wrapper flex-col justify-between h-full">
-               <p className="card__descr">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                  blanditiis nemo fugit autem possimus, magnam consequatur sint esse.
-               </p>
-               <div className="card__links">
-                  <div>
-                     <a className="link" href="#">
-                        Preview
-                     </a>
-                  </div>
-                  <div>
-                     <a className="link" href="#">
-                        Code
                      </a>
                   </div>
                </div>
