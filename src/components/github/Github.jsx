58ac033a349688903b4f5621bea8da7e29235e8e
Saif <droconos99@gmail.com>
2024-03-18 02:07:05 +0600

    added basic layout, debuggin left

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,39 +1,56 @@
 import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Github() {
-   const data = useLoaderData();
+const Card = (data, color) => {
    return (
-      <div className="flex justify-center flex-col md:flex-row items-center py-10">
-         <div className="card m-10 ">
-            <div className="card__img bg-blue-800"></div>
-            <p className="card__title text-3xl font-semibold">Project</p>
-            <div className="card__descr-wrapper flex-col justify-between h-full">
-               <p className="card__descr text-xl">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                  blanditiis nemo fugit autem possimus, magnam consequatur sint esse.
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam,
-                  deleniti labore beatae reiciendis iusto non eaque. Modi nisi odit,
-                  assumenda quas, temporibus nam magni aliquam facilis rem illum sapiente?
-                  Facilis!
-               </p>
-               <div className="card__links text-xl">
-                  <div className="flex flex-row">
-                     <img src="src/assets/icons8-github-96.png" className="w-7 h-7" />
-                     <a
-                        className="link text-blue-800 font-semibold flex items-center"
-                        href="#"
-                     >
-                        Source Code
-                     </a>
-                  </div>
-                  <div>
-                     <a className="link text-blue-800 font-semibold" href="#">
-                        Preview
-                     </a>
-                  </div>
+      <div className="card m-10 ">
+         <div className={`card__img ${color}`}></div>
+         <p className="card__title text-3xl font-semibold"> {data.name} </p>
+         <div className="card__descr-wrapper flex-col justify-between h-full">
+            <p className="card__descr text-xl">
+               {data.description}
+               {/* <br />
+               <span className="text-blue-800 font-semibold">Language: </span>
+               {data.language} */}
+            </p>
+            <div className="card__links text-xl">
+               <div className="flex flex-row">
+                  <img src="src/assets/icons8-github-96.png" className="w-7 h-7" />
+                  <Link
+                     className="link text-blue-800 font-semibold flex items-center"
+                     to={data.html_url}
+                  >
+                     Source Code
+                  </Link>
+               </div>
+               <div>
+                  <Link className="link text-blue-800 font-semibold" to={data.homepage}>
+                     Preview
+                  </Link>
                </div>
             </div>
          </div>
+      </div>
+   );
+};
+
+export default function Github() {
+   const data = useLoaderData();
+   const color = [
+      "bg-blue-800",
+      "bg-red-800",
+      "bg-yellow-800",
+      "bg-green-800",
+      "bg-indigo-800",
+      "bg-slate-800",
+      "bg-pink-800",
+      "bg-orange-800",
+   ];
+   return (
+      <div className="flex justify-center flex-col md:flex-row items-center py-10">
+         {data.map((item, index) => {
+            <Card data={item} color={color[index % 8]} />;
+         })}
       </div>
    );
 }
