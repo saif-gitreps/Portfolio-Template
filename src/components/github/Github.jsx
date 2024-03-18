154a6b4ed1a0220c3b5c0e0004b589ad1dc29914
Saif <droconos99@gmail.com>
2024-03-18 22:46:40 +0600

    align all cards and grid div

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,9 +1,9 @@
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-const Card = (data, color) => {
+const Card = ({ data, color }) => {
    return (
-      <div className="card m-10 ">
+      <div className="card m-8 self-center">
          <div className={`card__img ${color}`}></div>
          <p className="card__title text-3xl font-semibold"> {data.name} </p>
          <div className="card__descr-wrapper flex-col justify-between h-full">
@@ -36,7 +36,7 @@ const Card = (data, color) => {
 
 export default function Github() {
    const data = useLoaderData();
-   const color = [
+   const bgColor = [
       "bg-blue-800",
       "bg-red-800",
       "bg-yellow-800",
@@ -46,11 +46,14 @@ export default function Github() {
       "bg-pink-800",
       "bg-orange-800",
    ];
+
    return (
-      <div className="flex justify-center flex-col md:flex-row items-center py-10">
-         {data.map((item, index) => {
-            <Card data={item} color={color[index % 8]} />;
-         })}
+      <div className="flex justify-center w-screen m-auto">
+         <div className="flex justify-center flex-col md:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 items-center py-10">
+            {data.map((repo, index) => {
+               return <Card key={index} data={repo} color={bgColor[index % 8]} />;
+            })}
+         </div>
       </div>
    );
 }
